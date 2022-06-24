41f1199241176fef3e82efe2bd8103050b012006
Jason STRAZISAR <jason.strazisar67@gmail.com>
2022-06-24 17:41:55 +0200

    feat: 🎸 config stitches

--- src/lib/stitches.config.ts
+++ src/lib/stitches.config.ts
@@ -1,7 +1,21 @@
 import { createStitches, CSS as StitchesCSS } from "@stitches/react";
+import { palette } from "./tokens/colors";
+import { spaces } from "./tokens/spaces";
+import { fonts, fontSizes, fontWeights } from "./tokens/typography";
+import { shadows } from "./tokens/shadows";
 
 const { config, css, getCssText, globalCss, keyframes, styled, theme } =
-  createStitches();
+  createStitches({
+    theme: {
+      colors: palette,
+      space: spaces,
+      fonts: fonts,
+      fontSizes: fontSizes,
+      fontWeights: fontWeights,
+      shadows: shadows,
+      sizes: spaces,
+    },
+  });
 
 export type CSS = StitchesCSS<typeof config>;
 export type { VariantProps } from "@stitches/react";
